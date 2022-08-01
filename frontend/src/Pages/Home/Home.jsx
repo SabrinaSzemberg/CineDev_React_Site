723e2be279fac78c7052dbe646649409d74455bc
GustavoArcoverde <garcoverde3@gmail.com>
2022-08-01 14:27:42 -0300

    add imagens e links na home

--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
+import {Link} from "react-router-dom";
+import Icon1 from '../../assets/icon1.jpg'
+import Icon2 from '../../assets/icon2.jpg'
+import Icon3 from '../../assets/icon3.jpg'
+import Icon4 from '../../assets/icon4.jpg'
+import Icon5 from '../../assets/icon5.jpg'
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div>
+      <h1>Escolha sua entidade:</h1>
+      <nav>
+        <Link to='/'>
+          <img src={Icon1} alt="" />
+        </Link>
+        <Link to='/'>
+          <img src={Icon2} alt="" />
+        </Link>
+        <Link to='/'>
+          <img src={Icon3} alt="" />
+        </Link>
+        <Link to='/'>
+          <img src={Icon4} alt="" />
+        </Link>
+        <Link to='/'>
+          <img src={Icon5} alt="" />
+        </Link>
+      </nav>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
