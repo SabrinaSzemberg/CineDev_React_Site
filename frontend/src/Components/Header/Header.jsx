966fb7945af76f51edc610db6fdcce6b442d27cb
Sabrina Szemberg <sabrina.schenberg@gmail.com>
2022-08-03 19:03:09 -0300

    add header

---
+++ frontend/src/Components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import S from './Header.module.css'
+
+const Header = () => {
+  return (
+    <header className={S.header}>
+      <Link className="titulo" to="/">CineDev</Link>
+      <nav>
+      <Link className="crud" to="/">Post</Link>
+      <Link className="crud" to="/">Get</Link>
+      <Link className="crud" to="/">Get Id</Link>
+      <Link className="crud" to="/">Put</Link>
+      <Link className="crud" to="/">Delete</Link>
+      </nav>
+    </header>
+  )
+}
+
+export default Header
--- frontend/src/Routes.jsx
+++ frontend/src/Routes.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import { BrowserRouter, Routes as Switch, Route } from "react-router-dom";
 import Home from "./Pages/Home/Home";
+import Header from "./Components/Header/Header";
+import Delete from "./Pages/Delete/Delete"
 
 const Routes = () => {
   return (
     <BrowserRouter>
+    <Header/>
       <Switch>
+        {/* adicionar header */}
         <Route path='/' element={<Home/>}/>
+        <Route path='/Delete' element={<Delete/>}/>
       </Switch>
     </BrowserRouter>
   );
