2ecd6ae122e9e88372c5725795ca04a64cd7ce60
joao pedro <joaopedrobsi10@gmail.com>
2022-08-10 19:56:53 -0300

    atualizando

--- frontend/src/Pages/EditarCliente/EditarCliente.jsx
+++ frontend/src/Pages/EditarCliente/EditarCliente.jsx
@@ -2,9 +2,25 @@ import React, { useState, useEffect } from 'react'
 import {getClienteId, putClientes} from '../../Service/Api'
 import S from "./EditarCliente.module.css"
 import {useSearchParams} from "react-router-dom";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as yup from "yup";
 
 const EditarCliente = () => {
 
+    const validationSchema = yup.object({
+        nome: yup
+        .string()
+        
+        .required("O campo é obrigatório."),
+        email: yup
+          .string()
+          .email("Email invalido ")
+          .required("Email invalido"),
+        password: yup
+          .string()
+          .min(8, "Senha no  minimo com 8 caracteres")
+          .required("A senha é obrigatória")
+      });
     const [searchParams] = useSearchParams();
     const [dadosForm, setDadosForm] = useState({});
 
@@ -40,25 +56,32 @@ const EditarCliente = () => {
 
     <section className={S.content}>
         <section className={S.upperSection}>
+        <Formik
+      initialValues={{ nome:"" , email: "", password: "" }}
+      validationSchema={validationSchema}
+    >
             <form>
                 <label htmlFor="">Name</label>
-                <input
+                <Field
                     type="text"
                     value={dadosForm.name}
                     onChange={({target}) => handleDadosForm(target, "name")}
                 />
+                <ErrorMessage component="div" name="name" />
                 <label htmlFor="">Email</label>
                 <input
                     type="text"
                     value={dadosForm.email}
                     onChange={({target}) => handleDadosForm(target, "email")}
                 />
+                <ErrorMessage component="div" name="email" />
                 <label htmlFor="">Password</label>
-                <input
+                <Field
                     type="text"
                     value={dadosForm.password}
                     onChange={({target}) => handleDadosForm(target, "password")}
                 />
+                 <ErrorMessage component="div" name="email" />
                 <label htmlFor="">Payment</label>
                 <input
                     type="text"
@@ -72,6 +95,7 @@ const EditarCliente = () => {
                     onChange={({target}) => handleDadosForm(target, "club")}
                 />
             </form>
+            </Formik>
         </section>
         <section className={S.lowerSection}>
             <button onClick={()=>fazerPut()}>Salvar</button>
