b1fa16e29ac4e211256e6bd9716daa7db1d09327
joao pedro <joaopedrobsi10@gmail.com>
2022-08-10 20:14:11 -0300

    atualizando editar

--- frontend/src/Pages/EditarCliente/EditarCliente.jsx
+++ frontend/src/Pages/EditarCliente/EditarCliente.jsx
@@ -1,84 +1,103 @@
-import React, { useState, useEffect } from 'react'
-import {getClienteId, putClientes} from '../../Service/Api'
-import S from "./EditarCliente.module.css"
-import {useSearchParams} from "react-router-dom";
+import React, { useState } from 'react'
+import { postClientes } from '../../Service/Api'
+import S from "./AdicionarCliente.module.css"
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as yup from "yup";
 
-const EditarCliente = () => {
+const AdicionarCliente = () => {
 
-    const [searchParams] = useSearchParams();
-    const [dadosForm, setDadosForm] = useState({});
-
-    useEffect(() => requisition,[])
-
-    async function requisition() {
-        const response = await getClienteId(searchParams.get('id'));
-        const responseLoweredCaseKeys = parseToLowerOrUpperCase(response[0], false)
-        setDadosForm(responseLoweredCaseKeys)
-    }
+    const [dadosForms, setDados] = useState({
+        NAME:"",
+        EMAIL:"",
+        PASSWORD:"",
+        PAYMENT:"",
+        CLUB:""
+    });
+    
+    const validationSchema = yup.object({
+        nome: yup
+        .string()
+        
+        .required("O campo é obrigatório."),
+        email: yup
+          .string()
+          .email("Email invalido ")
+          .required("Email invalido"),
+        password: yup
+          .string()
+          .min(8, "Senha no  minimo com 8 caracteres")
+          .required("A senha é obrigatória")
+      });
 
     function handleDadosForm(target, nomeInput){
         const value = target.value;
-        setDadosForm({...dadosForm, [nomeInput]: value});
+        setDados({...dadosForms, [nomeInput]: value});
     }
 
-    function parseToLowerOrUpperCase(obj, upper) {
-        const entries = Object.entries(obj);
-
-        return Object.fromEntries(
-            entries.map(([key, value]) => {
-                return [upper ? key.toUpperCase() : key.toLowerCase(), value];
-            }),
-        );
-    }
-
-    const fazerPut = () => {
-        const toUpperKeys = parseToLowerOrUpperCase(dadosForm, true)
-        putClientes(toUpperKeys, dadosForm.id)
+    function fazerPost(){
+        postClientes(dadosForms)
     }
 
   return (
-
+    
     <section className={S.content}>
         <section className={S.upperSection}>
+        <Formik
+      initialValues={{ nome:"" , email: "", password: "" }}
+      validationSchema={validationSchema}
+    >
             <form>
                 <label htmlFor="">Name</label>
-                <input
+                <Field
                     type="text"
-                    value={dadosForm.name}
-                    onChange={({target}) => handleDadosForm(target, "name")}
-                />
+                    name="name"
+                    placeholder="Name"
+                    value={dadosForms.NAME}
+                    onChange={({target}) => handleDadosForm(target, "NAME")}/>
+                 <ErrorMessage component="div" name="name" />
                 <label htmlFor="">Email</label>
-                <input
-                    type="text"
-                    value={dadosForm.email}
-                    onChange={({target}) => handleDadosForm(target, "email")}
-                />
+                
+                <Field
+                    type="email" name="email" placeholder="Email" 
+                    value={dadosForms.EMAIL}
+                    onChange={({target}) => handleDadosForm(target, "EMAIL")}/>
+                    <ErrorMessage component="div" name="email" />
+                
+                
                 <label htmlFor="">Password</label>
-                <input
-                    type="text"
-                    value={dadosForm.password}
-                    onChange={({target}) => handleDadosForm(target, "password")}
-                />
+                
+                <Field
+                    type="password" name="password" placeholder="Password" 
+                    value={dadosForms.PASSWORD}
+                    onChange={({target}) => handleDadosForm(target, "PASSWORD")}/>
+                    <ErrorMessage component="div" name="password" />
+                   
                 <label htmlFor="">Payment</label>
                 <input
                     type="text"
-                    value={dadosForm.payment}
-                    onChange={({target}) => handleDadosForm(target, "payment")}
+                    placeholder="Payment"
+                    value={dadosForms.PAYMENT}
+                    onChange={({target}) => handleDadosForm(target, "PAYMENT")}
                 />
                 <label htmlFor="">Club</label>
                 <input
                     type="text"
-                    value={dadosForm.club}
-                    onChange={({target}) => handleDadosForm(target, "club")}
+                    placeholder="Club"
+                    value={dadosForms.CLUB}
+                    onChange={({target}) => handleDadosForm(target, "CLUB")}
                 />
+              
             </form>
+            </Formik>
+            
         </section>
         <section className={S.lowerSection}>
-            <button onClick={()=>fazerPut()}>Salvar</button>
+            <button  type="submit" onClick={()=>fazerPost()}>Salvar</button>
+          
         </section>
     </section>
-
+    
   )
 }
 
-export default EditarCliente
+export default AdicionarCliente
